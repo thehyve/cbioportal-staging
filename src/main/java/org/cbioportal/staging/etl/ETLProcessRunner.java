a017924909d6c2828149387d2b71f6017e93f265
oplantalech <oleguer@thehyve.nl>
2018-02-15 17:44:00 +0100

    Improve coding, add remaining headers

--- src/main/java/org/cbioportal/staging/etl/ETLProcessRunner.java
+++ src/main/java/org/cbioportal/staging/etl/ETLProcessRunner.java
@@ -1,3 +1,8 @@
+/*
+* Copyright (c) 2018 The Hyve B.V.
+* This code is licensed under the GNU Affero General Public License,
+* version 3, or (at your option) any later version.
+*/
 package org.cbioportal.staging.etl;
 
 import java.io.IOException;
