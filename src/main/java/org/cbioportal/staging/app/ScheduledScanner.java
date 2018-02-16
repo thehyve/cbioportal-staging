2079e61d066d5972d13a4f7e318e8f128460673a
oplantalech <oleguer@thehyve.nl>
2018-02-16 12:27:41 +0100

    Fix exceptions to stop cron job when necessary

--- src/main/java/org/cbioportal/staging/app/ScheduledScanner.java
+++ src/main/java/org/cbioportal/staging/app/ScheduledScanner.java
@@ -1,8 +1,8 @@
 /*
-* Copyright (c) 2018 The Hyve B.V.
-* This code is licensed under the GNU Affero General Public License,
-* version 3, or (at your option) any later version.
-*/
+ * Copyright (c) 2018 The Hyve B.V.
+ * This code is licensed under the GNU Affero General Public License,
+ * version 3, or (at your option) any later version.
+ */
 package org.cbioportal.staging.app;
 
 import java.io.IOException;
@@ -10,6 +10,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import org.cbioportal.staging.etl.ETLProcessRunner;
+import org.cbioportal.staging.services.EmailService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,8 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import com.amazonaws.services.s3.model.AmazonS3Exception;
 
 /**
  * Main app class to run the staging and loading process as a background service
@@ -28,54 +31,72 @@ import org.springframework.stereotype.Component;
 @Component
 public class ScheduledScanner 
 {
-    private static final Logger logger = LoggerFactory.getLogger(ScheduledScanner.class);
-    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+	private static final Logger logger = LoggerFactory.getLogger(ScheduledScanner.class);
+	private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
 
-    @Value("${scan.location}")
-    private String scanLocation;
-    
-    @Value("${scan.cron.iterations:-1}")
-    private Integer scanIterations;
-    private int nrIterations = 0;
+	@Value("${scan.location}")
+	private String scanLocation;
 
-    @Autowired
-    private ResourcePatternResolver resourcePatternResolver;
-    @Autowired
-    private ETLProcessRunner etlProcessRunner;
-    
+	@Value("${scan.cron.iterations:-1}")
+	private Integer scanIterations;
+	private int nrIterations = 0;
 
-    @Scheduled(cron = "${scan.cron}")
-    public boolean scan() throws IOException {
-        logger.info("Fixed Rate Task :: Execution Time - {}", dateTimeFormatter.format(LocalDateTime.now()) );
-        nrIterations++;
-        logger.info("Scanning location for new staging files: " + scanLocation);
-        Resource[] allFilesInFolder =  this.resourcePatternResolver.getResources(scanLocation + "/list_of_studies*.yaml");
-        logger.info("Found "+ allFilesInFolder.length + " index files");
+	@Autowired
+	private ResourcePatternResolver resourcePatternResolver;
+	@Autowired
+	private ETLProcessRunner etlProcessRunner;
+	@Autowired
+	EmailService emailService;
 
-        if (allFilesInFolder.length == 0)
-            return false;
-
-        Resource mostRecentFile = allFilesInFolder[0];
-        for (Resource resource : allFilesInFolder) {
-
-            if (resource.lastModified() > mostRecentFile.lastModified()) {
-                mostRecentFile = resource;
-            }
-        }
-        logger.info("Selected most recent one: "+ mostRecentFile.getFilename());
-
-        // trigger ETL process:
-        etlProcessRunner.run(mostRecentFile);
-
-        
-        //check if nrRepeats reached the configured max: 
-        if (scanIterations != -1 && nrIterations >= scanIterations) {
-            logger.info("==>>>>> Reached configured number of iterations (" + scanIterations + "). Exiting... <<<<<==");
-            System.exit(0);
-        }
-
-        //return true if a process was triggered
-        return true;
-    }
+	@Scheduled(cron = "${scan.cron}")
+	public boolean scan() {
+		try {
+			logger.info("Fixed Rate Task :: Execution Time - {}", dateTimeFormatter.format(LocalDateTime.now()) );
+			nrIterations++;
+			logger.info("Scanning location for new staging files: " + scanLocation);
+			Resource[] allFilesInFolder =  this.resourcePatternResolver.getResources(scanLocation + "/list_of_studies*.yaml");
+			logger.info("Found "+ allFilesInFolder.length + " index files");
+	
+			if (allFilesInFolder.length == 0)
+				return false;
+	
+			Resource mostRecentFile = allFilesInFolder[0];
+			for (Resource resource : allFilesInFolder) {
+	
+				if (resource.lastModified() > mostRecentFile.lastModified()) {
+					mostRecentFile = resource;
+				}
+			}
+			logger.info("Selected most recent one: "+ mostRecentFile.getFilename());
+	
+			// trigger ETL process:
+			etlProcessRunner.run(mostRecentFile);
+	
+	
+			//check if nrRepeats reached the configured max: 
+			if (scanIterations != -1 && nrIterations >= scanIterations) {
+				logger.info("==>>>>> Reached configured number of iterations (" + scanIterations + "). Exiting... <<<<<==");
+				System.exit(0);
+			}
+			
+		} catch (AmazonS3Exception e) {
+			logger.error("The bucket cannot be reached. Please check the scan.location provided in the application.properties.");
+			System.exit(1);
+		} catch (IOException e) {
+			System.exit(1);
+		} catch (Exception e) {
+			logger.error("An error not expected occurred. Stopping process...");
+			try {
+				emailService.emailGenericError("An error not expected occurred. Stopping process...", e);
+				System.exit(1);
+			} catch (Exception e1) {
+				logger.error("The email could not be sent due to the error specified below.");
+				e1.printStackTrace();
+			}
+			e.printStackTrace();
+		}
+		//return true if a process was triggered
+		return true;
+	}
 
 }
--- src/main/java/org/cbioportal/staging/etl/Loader.java
+++ src/main/java/org/cbioportal/staging/etl/Loader.java
@@ -6,6 +6,7 @@
 package org.cbioportal.staging.etl;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,6 +19,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import freemarker.core.ParseException;
+import freemarker.template.MalformedTemplateNameException;
+import freemarker.template.TemplateException;
+import freemarker.template.TemplateNotFoundException;
 
 @Component
 public class Loader {
@@ -47,40 +53,29 @@ public class Loader {
 	@Value("${portal.home}")
 	private String portalHome;
 	
-	void load(Integer id, List<String> studies) {
-		try {
-			if (!centralShareLocation.exists()) {
-				throw new Exception("The central share location directory specified in application.properties do not exist: "+centralShareLocation.toString()+
-						". Stopping process...");
-			} else {
-				Map<String, String> statusStudies = new HashMap<String, String>();
-				//Get studies from appropriate staging folder
-				File originPath = new File(etlWorkingDir.toPath()+"/"+id+"/staging");
-				for (String study : studies) {
-					try {
-						logger.info("Starting loading of study "+study);
-						File studyPath = new File(originPath+"/"+study);
-						File logFile = loaderService.load(study, studyPath);
-						logger.info("Loading of study "+study+" finished.");
-						statusStudies.put(logFile.getAbsolutePath(), "<b><font style=\"color: #04B404\">SUCCESSFULLY LOADED</font></b>");
-					} catch (Exception e) {
-						//tell about error, continue with next study
-						logger.error(e.getMessage()+". The app will continue with the next study.");
-						statusStudies.put(study, "<b><font style=\"color: #FF0000\">ERRORS</font></b>");
-						e.printStackTrace();
-					}
+	void load(Integer id, List<String> studies) throws TemplateNotFoundException, MalformedTemplateNameException, ParseException, IOException, TemplateException {
+		if (!centralShareLocation.exists()) {
+			throw new IOException("The central share location directory specified in application.properties do not exist: "+centralShareLocation.toString()+
+					". Stopping process...");
+		} else {
+			Map<String, String> statusStudies = new HashMap<String, String>();
+			//Get studies from appropriate staging folder
+			File originPath = new File(etlWorkingDir.toPath()+"/"+id+"/staging");
+			for (String study : studies) {
+				try {
+					logger.info("Starting loading of study "+study);
+					File studyPath = new File(originPath+"/"+study);
+					File logFile = loaderService.load(study, studyPath);
+					logger.info("Loading of study "+study+" finished.");
+					statusStudies.put(logFile.getAbsolutePath(), "<b><font style=\"color: #04B404\">SUCCESSFULLY LOADED</font></b>");
+				} catch (Exception e) {
+					//tell about error, continue with next study
+					logger.error(e.getMessage()+". The app will continue with the next study.");
+					statusStudies.put(study, "<b><font style=\"color: #FF0000\">ERRORS</font></b>");
+					e.printStackTrace();
 				}
-				emailService.emailStudiesLoaded(statusStudies);
 			}
-		} catch (Exception e) {
-			logger.error("An error not expected occurred. Stopping process...");
-			try {
-				emailService.emailGenericError("An error not expected occurred. Stopping process...", e);
-			} catch (Exception e1) {
-				logger.error("The email could not be sent due to the error specified below.");
-				e1.printStackTrace();
-			}
-			e.printStackTrace();
+			emailService.emailStudiesLoaded(statusStudies);
 		}
 	}
 }
