d12c746c8bd1e813332bb93afb25090b118a6805
Pieter Lukasse <pieterlukasse@gmail.com>
2018-03-08 12:15:24 +0100

    simplified restart parameters

--- src/main/java/org/cbioportal/staging/services/RestarterServiceImpl.java
+++ src/main/java/org/cbioportal/staging/services/RestarterServiceImpl.java
@@ -33,48 +33,44 @@ public class RestarterServiceImpl implements RestarterService {
 
 	@Value("${cbioportal.mode}")
 	private String cbioportalMode;
-	
-	@Value("${refresh.command}")
-	private String refreshCommand;
 
+	@Value("${cbioportal.docker.cbio.container}")
+	private String cbioContainer;
+	
 	@Value("${portal.home:.}")
 	private String portalHome;
 	
 	@Override
 	public void restart() throws InterruptedException, IOException, ConfigurationException {
-		if (!refreshCommand.equals("")) {
-			if (cbioportalMode.equals("local")) {
-				logger.info("Stopping Tomcat...");
-				ProcessBuilder stopCmd = new ProcessBuilder(refreshCommand, "stop", "-force");
-				stopCmd.directory(new File(portalHome));
-				logger.info("Executing command: "+String.join(" ", stopCmd.command()));
-				Process stopProcess = stopCmd.start();
-				stopProcess.waitFor();
-				logger.info("Tomcat successfully stopped. Restarting Tomcat...");
-				ProcessBuilder startCmd = new ProcessBuilder(refreshCommand, "start");
-				startCmd.directory(new File(portalHome));
-				Process startProcess = startCmd.start();
-				startProcess.waitFor();
-				logger.info("Tomcat successfully restarted.");
-			} else if (cbioportalMode.equals("docker")) {
-				logger.info("Restarting Tomcat...");
-				ProcessBuilder restarterCmd = new ProcessBuilder ("docker", "restart", refreshCommand);
-				logger.info("Executing command: "+String.join(" ", restarterCmd.command()));
-				Process restartProcess = restarterCmd.start();
-				BufferedReader reader = new BufferedReader(new InputStreamReader(restartProcess.getErrorStream()));
-				String line = null;
-				while ((line = reader.readLine()) != null)
-				{
-					logger.info(line); //Print output if an error occurs
-				}
-				restartProcess.waitFor();
-				logger.info("Tomcat successfully restarted.");
-			} else {
-				throw new ConfigurationException("cbioportal.mode is not 'local' or 'docker'. Value encountered: "+cbioportalMode+
-						". Please change the mode in the application.properties.");
+		if (cbioportalMode.equals("local")) {
+			logger.info("Stopping Tomcat...");
+			ProcessBuilder stopCmd = new ProcessBuilder("catalina", "stop", "-force");
+			stopCmd.directory(new File(portalHome));
+			logger.info("Executing command: "+String.join(" ", stopCmd.command()));
+			Process stopProcess = stopCmd.start();
+			stopProcess.waitFor();
+			logger.info("Tomcat successfully stopped. Restarting Tomcat...");
+			ProcessBuilder startCmd = new ProcessBuilder("catalina", "start");
+			startCmd.directory(new File(portalHome));
+			Process startProcess = startCmd.start();
+			startProcess.waitFor();
+			logger.info("Tomcat successfully restarted.");
+		} else if (cbioportalMode.equals("docker")) {
+			logger.info("Restarting Tomcat...");
+			ProcessBuilder restarterCmd = new ProcessBuilder ("docker", "restart", cbioContainer);
+			logger.info("Executing command: "+String.join(" ", restarterCmd.command()));
+			Process restartProcess = restarterCmd.start();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(restartProcess.getErrorStream()));
+			String line = null;
+			while ((line = reader.readLine()) != null)
+			{
+				logger.info(line); //Print output if an error occurs
 			}
+			restartProcess.waitFor();
+			logger.info("Tomcat successfully restarted.");
 		} else {
-			throw new IOException("The refresh.command is empty. Please check your application.properties.");
+			throw new ConfigurationException("cbioportal.mode is not 'local' or 'docker'. Value encountered: "+cbioportalMode+
+					". Please change the mode in the application.properties.");
 		}
 	}
 
