b18de62cf36037b8d8bc852086086c64cb621877
Pim van Nierop <pim@thehyve.nl>
2020-01-28 15:19:23 +0100

    WIP

--- src/test/java/org/cbioportal/staging/etl/FullIntegrationTest.java
+++ src/test/java/org/cbioportal/staging/etl/FullIntegrationTest.java
@@ -3,6 +3,7 @@ package org.cbioportal.staging.etl;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyInt;
 import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -12,7 +13,10 @@ import java.util.Map;
 
 import org.cbioportal.staging.app.App;
 import org.cbioportal.staging.app.ScheduledScanner;
+import org.cbioportal.staging.exceptions.ConfigurationException;
 import org.cbioportal.staging.services.EmailServiceImpl;
+import org.cbioportal.staging.services.RestarterService;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,25 +31,28 @@ import freemarker.template.TemplateException;
 import freemarker.template.TemplateNotFoundException;
 
 @RunWith(SpringRunner.class)
-@TestPropertySource(properties = {
-    "scan.location=file:src/test/resources/e2e_studies/es_0",
-    "etl.working.dir=file:/tmp/staging-integration-test/etl-working-dir",
-    "cbioportal.mode=docker",
-    "cbioportal.docker.image=cbioportal/cbioportal:3.1.4",
-    "cbioportal.docker.network=cbio-net",
-    "cbioportal.docker.properties=/tmp/staging-integration-test/portal.properties",
-    "central.share.location=file:/tmp/staging-integration-test/share",
-    "skip.transformation=true",
-    "cloud.aws.region.static=eu-central-1"
-})
+@TestPropertySource(properties = { "scan.location=file:src/test/resources/e2e_studies/es_0",
+        "etl.working.dir=file:/tmp/staging-integration-test/etl-working-dir", "cbioportal.mode=docker",
+        "cbioportal.docker.image=cbioportal/cbioportal:3.1.4", "cbioportal.docker.network=cbio-net",
+        "cbioportal.docker.properties=/tmp/staging-integration-test/portal.properties",
+        "cbioportal.docker.cbio.container=cbio", "central.share.location=file:/tmp/staging-integration-test/share",
+        "skip.transformation=true", "cloud.aws.region.static=eu-central-1" })
 @SpringBootTest(classes = App.class)
 public class FullIntegrationTest {
 
     @MockBean
     private EmailServiceImpl emailServiceImpl;
 
+    @MockBean
+    private RestarterService restarterService;
+
     @Autowired
     private ScheduledScanner scheduledScanner;
+
+    @Before
+    public void init() throws InterruptedException, IOException, ConfigurationException {
+        doNothing().when(restarterService).restart();
+    }
 
     @Test
     public void loadSuccessful_es0() throws TemplateNotFoundException, MalformedTemplateNameException, ParseException,
---
+++ src/test/resources/e2e_studies/es_0_tar/unzip.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+# Wrapper to transform staging files for cBioPortal
+# Author: Oleguer Plantalech, Sander Tan, Dionne Zaal, Sander Rodenburg, The Hyve
+
+import os
+import argparse
+import sys
+
+def transform_study(study_dir, output_dir):
+    bash_command = "tar -C " + output_dir + " -pxvzf " + study_dir + "/test_study_es_0.tar.gz"
+    os.system(bash_command)
+    sys.exit(0)
+
+
+def main(study_dir, output_dir):
+    transform_study(study_dir, output_dir)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(usage="-i <dir_for_input_files> -o <dir_for_output_files>",
+                                     description="Transforms all files for all studies in input folder to cBioPortal "
+                                                 "staging files")
+
+    arguments = parser.add_argument_group('Named arguments')
+
+    arguments.add_argument("-i", "--input_dir",
+                           required=True,
+                           help="Directory containing input files.")
+
+    arguments.add_argument("-o", "--output_dir",
+                           required=True,
+                           help="Directory for output files.")
+
+    args = parser.parse_args()
+    main(args.input_dir, args.output_dir)
