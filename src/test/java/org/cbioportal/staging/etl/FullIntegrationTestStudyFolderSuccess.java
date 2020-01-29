0eaa8fa9c786093f489bc1b2c7e419e74bd2783b
Pim van Nierop <pim@thehyve.nl>
2020-01-29 14:31:30 +0100

    WIP

--- src/test/java/org/cbioportal/staging/etl/FullIntegrationTestStudyFolderSuccess.java
+++ src/test/java/org/cbioportal/staging/etl/FullIntegrationTestStudyFolderSuccess.java
@@ -16,7 +16,6 @@ import org.cbioportal.staging.app.ScheduledScanner;
 import org.cbioportal.staging.exceptions.ConfigurationException;
 import org.cbioportal.staging.services.EmailServiceImpl;
 import org.cbioportal.staging.services.RestarterService;
-import org.cbioportal.staging.services.TransformerServiceImpl;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -25,7 +24,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.util.ReflectionTestUtils;
 
 import freemarker.core.ParseException;
 import freemarker.template.MalformedTemplateNameException;
@@ -34,15 +32,8 @@ import freemarker.template.TemplateNotFoundException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = App.class)
-@TestPropertySource(
-    locations = "file:src/test/resources/e2e_studies/e2e_integration_test.properties",
-    properties = {
-        "scan.location=file:src/test/resources/e2e_studies/es_0_tar",
-        "skip.transformation=false",
-        "transformation.command.script=file:src/test/resources/e2e_studies/es_0_tar/unzip.py"
-    }
-)
-public class FullIntegrationTestTransformation {
+@TestPropertySource(locations = "classpath:src/test/resources/e2e_studies/e2e_integration_test.properties")
+public class FullIntegrationTestStudyFolderSuccess {
 
     @MockBean
     private EmailServiceImpl emailServiceImpl;
@@ -53,38 +44,18 @@ public class FullIntegrationTestTransformation {
     @Autowired
     private ScheduledScanner scheduledScanner;
 
-    @Autowired
-    private TransformerServiceImpl transformerService;
-
     @Before
     public void init() throws InterruptedException, IOException, ConfigurationException {
         doNothing().when(restarterService).restart();
     }
 
     @Test
-    public void transformSuccessful_es0() throws TemplateNotFoundException, MalformedTemplateNameException, ParseException,
+    public void loadSuccessful_es0() throws TemplateNotFoundException, MalformedTemplateNameException, ParseException,
             IOException, TemplateException {
         boolean exitValue = scheduledScanner.scan();
         assert(exitValue);
         verify(emailServiceImpl, never()).emailStudyFileNotFound(any(Map.class),anyInt());
-        verify(emailServiceImpl, times(1)).emailTransformedStudies(any(Map.class),any(Map.class));
-        verify(emailServiceImpl, times(1)).emailValidationReport(any(Map.class),anyString(),any(Map.class));
-        verify(emailServiceImpl, times(1)).emailStudiesLoaded(any(Map.class),any(Map.class));
-        verify(emailServiceImpl, never()).emailGenericError(any(),any());
-    }
-
-    @Test
-    public void transformFailure_es0() throws TemplateNotFoundException, MalformedTemplateNameException, ParseException,
-            IOException, TemplateException {
-
-        // wire in a failing transformation script
-        ReflectionTestUtils.setField(transformerService,
-            "transformationCommandScript", "file:src/test/resources/e2e_studies/es_0_tar/exit1.py");
-
-        boolean exitValue = scheduledScanner.scan();
-        assert(exitValue);
-        verify(emailServiceImpl, never()).emailStudyFileNotFound(any(Map.class),anyInt());
-        verify(emailServiceImpl, times(1)).emailTransformedStudies(any(Map.class),any(Map.class));
+        verify(emailServiceImpl, never()).emailTransformedStudies(any(Map.class),any(Map.class));
         verify(emailServiceImpl, times(1)).emailValidationReport(any(Map.class),anyString(),any(Map.class));
         verify(emailServiceImpl, times(1)).emailStudiesLoaded(any(Map.class),any(Map.class));
         verify(emailServiceImpl, never()).emailGenericError(any(),any());
