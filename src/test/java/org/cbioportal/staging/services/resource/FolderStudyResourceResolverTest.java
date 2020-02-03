99195e8ca8d1f1aa7ea68f1c49a05d08601abca5
Pim van Nierop <pim@thehyve.nl>
2020-02-03 11:56:06 +0100

    Add @ActiveProfles annotation to test

--- src/test/java/org/cbioportal/staging/services/resource/FolderStudyResourceResolverTest.java
+++ src/test/java/org/cbioportal/staging/services/resource/FolderStudyResourceResolverTest.java
@@ -21,9 +21,11 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.Resource;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@ActiveProfiles("studydir")
 public class FolderStudyResourceResolverTest {
 
     @TestConfiguration
