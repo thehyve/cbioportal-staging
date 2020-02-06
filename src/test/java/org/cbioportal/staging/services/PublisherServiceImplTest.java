12350bf26dd8c6f8f0acdf851645905c143afcec
oplantalech <oleguer@thehyve.nl>
2020-02-06 11:50:15 +0100

    Fix Publisher tests

--- src/test/java/org/cbioportal/staging/services/PublisherServiceImplTest.java
+++ src/test/java/org/cbioportal/staging/services/PublisherServiceImplTest.java
@@ -7,10 +7,12 @@ import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import org.cbioportal.staging.exceptions.DirectoryCreatorException;
 import org.cbioportal.staging.exceptions.PublisherException;
 import org.cbioportal.staging.exceptions.ResourceCollectionException;
 import org.cbioportal.staging.services.resource.ResourceUtils;
@@ -19,12 +21,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.core.io.Resource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = PublisherServiceImpl.class,
+@SpringBootTest(classes = {PublisherServiceImpl.class, DirectoryCreatorByJob.class},
 properties = {
     "central.share.location=file:/fake-share/"
 })
@@ -33,19 +36,22 @@ public class PublisherServiceImplTest {
     @Autowired
     private PublisherServiceImpl publisherService;
 
+    @MockBean
+	private IDirectoryCreator directoryCreator;
+
     @SpyBean
     private ResourceUtils utils;
 
     @Test
-    public void testPublish_success() throws ResourceCollectionException, PublisherException {
+    public void testPublish_success() throws ResourceCollectionException, PublisherException, DirectoryCreatorException {
 
         Resource fakeShareLocation = TestUtils.createMockResource("file:/fake-share/", 0);
         doReturn(fakeShareLocation).when(utils).createDirResource(any(Resource.class), eq("dummy-date"));
-        // when(utils.createDirResource(any(Resource.class), anyString())).thenReturn(fakeShareLocation);
+        when(directoryCreator.getCentralShareLocationPath(any(Resource.class), anyString())).thenReturn(fakeShareLocation);
 
         Resource fakePublishedLogFile = TestUtils.createMockResource("file:/fake-share/log1.txt", 0);
         doReturn(fakePublishedLogFile).when(utils).copyResource(any(Resource.class), any(Resource.class), anyString());
-        // when(utils.copyResource(any(Resource.class), any(Resource.class), anyString())).thenReturn(fakePublishedLogFile);
+        when(utils.copyResource(any(Resource.class), any(Resource.class), anyString())).thenReturn(fakePublishedLogFile);
 
         Map<String,Resource> logFiles = new HashMap<>();
         Resource fakeLogFile = TestUtils.createMockResource("file:/logdir/log1.txt", 0);
